790d1a7cd1549fae06e979b27600b6f4e149b71b
Nolan Byrnes <nolanehs@gmail.com>
2018-01-11 07:42:31 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,8 +67,8 @@ end
 
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
-  Movie.create(title: "Wat?")
-  __
+  movie = Movie.create(title: "Wat?")
+  movie.update_attribute(:title, "Wat, huh?")
   __
 end
 
